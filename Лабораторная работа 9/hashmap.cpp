daebd10969e025056e66481a27009afe47006e24
Ivan-Ivashkin <ivashkinir@yandex.ru>
2021-04-05 13:07:41 +0300

    Начало работы

---
+++ Лабораторная работа 9/hashmap.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+struct Node
+{
+    int key;
+    int value;
+    Node * next;
+
+};
+
+struct HashMap
+{
+    int key;
+    int size;
+    int n_elements;
+    Node * table[];
+
+    void add(int key, int value)
+    {
+
+    }
+
+    int get(int key)
+    {
+
+    }
+
+    void reallocate()
+    {
+
+    }
+
+    int pop(key)
+    {
+
+    }
+
+};
+
+int main() {
+
+    return 0;
+}
