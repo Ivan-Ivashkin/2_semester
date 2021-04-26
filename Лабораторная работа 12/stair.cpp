dfcf6b4d55b65eb86be627680c7fc44e1ae8e3a7
Ivan-Ivashkin <ivashkinir@yandex.ru>
2021-04-26 20:39:05 +0300

    Лесенка (доработана)

--- Лабораторная работа 12/stair.cpp
+++ Лабораторная работа 12/stair.cpp
@@ -13,19 +13,37 @@ int main(){
 
     vector <int> stair(N+1, 0);
     vector <int> table(N+1, 0);
+    vector <bool> num(N+1, false);
 
     for (int i = 1; i <= N; i++) {
         cin >> stair[i];
     }
 
     table[1] = stair[1];
-    table[2] = max(stair[1], stair[2]);
+    table[2] = stair[2];
 
-    cout << '\n';
-
-    for (int i = 3; i <= N+1; i++) {
+    for (int i = 3; i <= N; i++) {
         table[i] = stair[i] + max(table[i-1], table[i-2]);
     }
 
+    int i = N;
+    while (i > 1){
+        if (max(table[i-1], table[i-2]) == table[i-1]) {
+            num[i-1] = true;
+            i = i-1;
+        } else {
+            num[i-2] = true;
+            i = i-2;
+        }
+    }
+    num[N] = true;
+
+    cout << '\n';
     cout << table[N] << '\n';
+
+    for (int i = 1; i <= N; i++){
+        if(num[i]){
+            cout << i << ' ';
+        }
+    }
 }
